70f5c4444d5619a44d44676d5525963afd271149
jbukuts <jbukuts@gmail.com>
2023-01-14 01:21:03 +0000

    Deploying to gh-pages from @ jbukuts/gatsby-contentful-blog@713412540483d3b93a9a252ee86d727c7ad9d65c 🚀

---
+++ 79826-63130ae8a35d6dd0a35f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_blog=self.webpackChunkgatsby_blog||[]).push([[79826],{79826:function(e,t,r){r.r(t);var s=r(67294),o=r(45697),n=r.n(o);const l=e=>{const{color:t,size:r,...o}=e;return s.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:r,height:r,viewBox:"0 0 24 24",fill:t,...o},s.createElement("path",{d:"M19,11H5a1,1,0,0,0,0,2H19a1,1,0,0,0,0-2Z"}))};l.propTypes={color:n().string,size:n().oneOfType([n().string,n().number])},l.defaultProps={color:"currentColor",size:"24"},t.default=l}}]);
+//# sourceMappingURL=79826-63130ae8a35d6dd0a35f.js.map
